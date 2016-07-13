fc5c3002213ff7e489bb67b800e73c140ac1e465
Conall Heffernan <conall.heffernan@logentries.com>
2016-07-13 16:27:18 +0100

    Update query_all_logs.py

--- query_all_logs.py
+++ query_all_logs.py
@@ -4,8 +4,6 @@ import sys
 import requests
 import time
 import csv
-import time
-from datetime import datetime
 
 
 API_KEY = sys.argv[2]
@@ -21,7 +19,7 @@ ACCOUNT_KEY = ''
 HOST_NAMES_KEYS_DICT = {}
 OUTFILE = open(sys.argv[3], 'w')
 OUTFILE_WRITER = csv.writer(OUTFILE)
-OUTFILE_WRITER.writerow(['Log Name', 'Query Result'])
+OUTFILE_WRITER.writerow(['Log Set', 'Log Name', 'Query Result'])
 
 
 def get_host_name():
@@ -31,8 +29,9 @@ def get_host_name():
         HOST_NAMES_KEYS_DICT[hosts['key']] = hosts['name']
     for k, v in HOST_NAMES_KEYS_DICT.iteritems():
         if v != r'Inactivity Alerts':
-            del v
-            get_log_name_and_key(k)
+            get_log_name_and_key(k, v)
+        else:
+            pass
 
 
 def get_le_url(url):
@@ -52,44 +51,58 @@ def get_continuity_final_response(response):
             continue
 
 
-def handle_response(resp):
-    time.sleep(4)
+def post_query_to_le(hostkey):
+    headers = {'x-api-key': API_KEY}
+    payload = {"logs": [hostkey],
+               "leql": {"during": {"from": FROM_TS, "to": TO_TS},
+                        "statement": SEARCH_QUERY}}
+
+    url = "https://rest.logentries.com/query/logs/"
+    return requests.post(url, headers=headers, json=payload)
+
+
+def handle_response(resp, log_key):
+    time.sleep(0.5)
     if resp.status_code == 200:
         return resp
-    if resp.status_code == 202:
+    elif resp.status_code == 202:
+        print "Polling after 202"
         return get_continuity_final_response(resp)
-    if resp.status_code > 202:
+    elif resp.status_code == 503:
+        print "Retrying after 503 code"
+        retried_response = post_query_to_le(log_key)
+        return handle_response(retried_response, log_key)
+    elif resp.status_code > 202:
         print 'Error status code ' + str(resp.status_code)
         return
 
 
-def get_log_name_and_key(host_key):
+def get_log_name_and_key(host_key, host_name):
     req = urllib.urlopen("http://api.logentries.com/" + ACCOUNT_KEY + '/hosts/' + host_key + '/')
     response = json.load(req)
-    headers = {'x-api-key': API_KEY}
-
     for everylogkey in response['list']:
+        if not everylogkey['key']:
+            continue
 
         print everylogkey['name']
 
-        payload = {"logs": [str(everylogkey['key'])],
-                   "leql": {"during": {"from": FROM_TS, "to": TO_TS},
-                            "statement": SEARCH_QUERY}}
-
-        url = "https://rest.logentries.com/query/logs/"
-        results = requests.post(url, headers=headers, json=payload)
-        results = handle_response(results)
+        results1 = post_query_to_le(str(everylogkey['key']))
+        results = handle_response(results1, str(everylogkey['key']))
         # if query is calculate(count) then: results.json()['statistics']['stats']['global_timeseries']['count']
         # if query is calculate(bytes) then: results.json()['statistics']['stats']['global_timeseries']['bytes']
         try:
-            query_result = str(results.json()['statistics']['stats']['global_timeseries']['bytes'])
+            if len(results.json()['statistics']['stats']['global_timeseries']) > 0:
+                query_result = str(results.json()['statistics']['stats']['global_timeseries']['bytes'])
+            else:
+                query_result = 0
             print query_result
-            OUTFILE_WRITER.writerow((everylogkey['name'], query_result))
+            OUTFILE_WRITER.writerow((host_name, everylogkey['name'], query_result))
         except KeyError as exception:
-            print "*** Empty Result ***"
+            print "Empty"
 
 
 if __name__ == '__main__':
     ACCOUNT_KEY = sys.argv[1]
     get_host_name()
     OUTFILE.close()
+    print "csv result file generated. all done."
